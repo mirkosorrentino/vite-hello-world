39e6d889a99953ada6ae12fe91d9111244d7309a
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-12 16:22:06 +0200

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "AppTitle",
+    data(){
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ title }} </h1>
+</template>
+
+<style>
+h1 {
+    font-size: 3rem;
+    font-family: sans-serif;
+    color: red;
+    font-weight: 700;
+    text-align: center;
+}
+</style>
